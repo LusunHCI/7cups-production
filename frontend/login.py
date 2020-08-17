abb44f6c6d074bbfecc0cc31f919b82993294de5
刘予晗 <liuyuhan@liuyuhandeMacBook-Pro.local>
2020-08-17 02:18:30 -0400

    fixed bugs on submiting when message count is larger than 10.

--- frontend/login.py
+++ frontend/login.py
@@ -5,47 +5,26 @@ import mysql.connector
 from datetime import datetime
 import re
 
-# app = Flask(__name__)
-
-# @app.route('/')
-
-# def hello_world():
-# 	return ('Hello, World!')
-
 app = Flask(__name__,static_folder='static',template_folder='static')
 
 @app.route('/',methods=['POST','GET'])
 def index():
 	return render_template('login.html')
 
-# from flask import Flask, escape, url_for, request, render_template, redirect, request
-# import requests
-# import json
-# import mysql.connector
-# from datetime import datetime
-# import re
-
-# app = Flask(__name__,static_folder='static',template_folder='static')
-
-# @app.route('/',methods=['POST','GET'])
-# def index():
-# 	return render_template('login.html')
-
 @app.route('/experience/?<string:userid>')
 def experience(userid):
     return render_template('experience.html')
 
-@app.route('/userMessage', methods=['POST','GET'])
+@app.route('/userMessage/', methods=['POST','GET'])
 def userMessage():
 	if request.method =='GET':
 		print("get")
 	if request.method =='POST':
 		mydb = mysql.connector.connect(
-  		host="db",
-		port=3306,
+  		host="localhost",
   		user="root",
-  		password="lusun",
-  		database="7cups"
+  		password="Lyh19970515@",
+  		database="7cups_summer"
 		)
 		mycursor = mydb.cursor()
 		message_id=request.json['message_id']
@@ -62,17 +41,16 @@ def userMessage():
 		mydb.close()
 	return render_template('experience.html')
 
-@app.route('/botResponse', methods=['POST','GET'])
+@app.route('/botResponse/', methods=['POST','GET'])
 def botResponse():
 	if request.method =='GET':
 		print("get")
 	if request.method =='POST':
 		mydb = mysql.connector.connect(
-  		host="db",
-		port=3306,
+  		host="localhost",
   		user="root",
-  		password="lusun",
-  		database="7cups"
+  		password="Lyh19970515@",
+  		database="7cups_summer"
 		)
 		mycursor = mydb.cursor()
 		message_id=request.json['message_id']
@@ -94,17 +72,16 @@ def botResponse():
 		mycursor.close()
 		mydb.close()
 	return render_template('experience.html')
-@app.route('/submitCodesign', methods=['POST','GET'])
+@app.route('/submitCodesign/', methods=['POST','GET'])
 def submitCodesign():
 	if request.method =='GET':
 		print("get")
 	if request.method =='POST':
 		mydb = mysql.connector.connect(
-  	  	host="db",
-		port=3306,
+  		host="localhost",
   		user="root",
-  		password="lusun",
-  		database="7cups"
+  		password="Lyh19970515@",
+  		database="7cups_summer"
 		)
 		mycursor = mydb.cursor()
 		form_json = json.loads(request.data)
@@ -118,24 +95,35 @@ def submitCodesign():
 			print(str(key) + '   ' + str(value))
 			if value=='':
 				value='null'
-			mid=re.findall(r"\d",key)[0]
-			field=key.replace(mid,'')
+			mid=re.findall(r"\d",key)
+			num=""
+			for n in range(0,len(mid)):
+				num+=mid[n]
+			print(num)
+			field=key.replace(num,'')
 			print(field)
-			if userid+mid not in msgid:
-				msgid.append(userid+mid)
+			if field=='intentselect':
+				if value!='Add_More':
+					value=value.split(":")[0]
+					print(value)
+			if userid+num not in msgid:
+				msgid.append(userid+num)
 				sql = "INSERT INTO codesign (message_id, chatroom_id ,"+field+") VALUES (%s,%s,%s)"
-				val=(userid+mid,userid,value)
+				val=(userid+num,userid,value)
 				mycursor.execute(sql,val)
 			else:
 				value=value.replace("'","_")
-				sql = "UPDATE codesign set "+field+"='"+value+"' where message_id="+(userid+mid)
+				sql = "UPDATE codesign set "+field+"='"+value+"' where message_id="+(userid+num)
 				mycursor.execute(sql)
 		mydb.commit()
 		mycursor.close()
 		mydb.close()
 	return render_template('experience.html')
 
-if __name__ == '__main__':
-	app.run(debug=True, host='0.0.0.0')
+
+
+
+
+
 
 
